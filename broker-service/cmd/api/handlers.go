939aa8150987738c8a1da989b1a3300cc9e9474f
tepavcevic <djordje.tepa@gmail.com>
2023-12-10 17:35:35 +0100

    Fix bug in handler, add dockerfile

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -17,8 +17,8 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Broker working great!",
 	}
 
-	output, _ := json.MarshalIndent(payload, "", "/t")
-	w.Header().Set("Content-Type", "appliaction/json")
+	output, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 	w.Write(output)
 }
