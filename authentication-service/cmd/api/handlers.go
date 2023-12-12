c8eee4d1413e6243ad4832592e1f0958288366da
tepavcevic <djordje.tepa@gmail.com>
2023-12-12 20:59:22 +0100

    Fix status code for unauthorized user

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -19,13 +19,13 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 
 	user, err := app.Models.User.GetByEmail(requestPayload.Email)
 	if err != nil {
-		app.errorJSON(w, r, errors.New("invalid credentials"), http.StatusBadRequest)
+		app.errorJSON(w, r, errors.New("invalid credentials"), http.StatusUnauthorized)
 		return
 	}
 
 	valid, err := user.PasswordMatches(requestPayload.Password)
 	if !valid || err != nil {
-		app.errorJSON(w, r, errors.New("invalid credentials"), http.StatusBadRequest)
+		app.errorJSON(w, r, errors.New("invalid credentials"), http.StatusUnauthorized)
 		return
 	}
 
