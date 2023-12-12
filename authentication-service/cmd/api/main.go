759f7f99e613eaa80ec3d9de1290ab033f0a4613
tepavcevic <djordje.tepa@gmail.com>
2023-12-12 13:34:21 +0100

    Create connection to the database in auth service

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/tepavcevic/microservices-golang/authentication/data"
 )
 
 const webPort = "8080"
+
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -18,7 +25,16 @@ type Config struct {
 
 func main() {
 	log.Println("Starting auth service")
-	app := Config{}
+
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to postgres")
+	}
+
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -26,5 +42,42 @@ func main() {
 	}
 	if err := srv.ListenAndServe(); err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		conn, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not ready...")
+			counts++
+		} else {
+			log.Println("Connected to postgres")
+			return conn
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for 2 seconds...")
+		time.Sleep(time.Second * 2)
+		continue
 	}
 }
