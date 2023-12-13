dd45c1616640ded856ad8dd9df7b029bace06af5
tepavcevic <djordje.tepa@gmail.com>
2023-12-13 18:06:26 +0100

    Init service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,12 @@
+package main
+
+const (
+	webPort  = "8080"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	grpcPort = "50001"
+)
+
+func main() {
+
+}
---
+++ logger-service/go.mod
@@ -0,0 +1,3 @@
+module github.com/tepavcevic/microservices-golang/logger
+
+go 1.21.1
