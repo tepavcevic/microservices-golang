b24ad561e59257129292e1238cda7695a43b5914
tepavcevic <djordje.tepa@gmail.com>
2023-12-15 14:05:33 +0100

    Finish setting up smtp server and handler

---
+++ mail-service/cmd/api/handlers.go
@@ -0,0 +1,38 @@
+package main
+
+import "net/http"
+
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+
+	var requestPayload mailMessage
+
+	if err := app.readJSON(w, r, &requestPayload); err != nil {
+		app.errorJSON(w, r, err)
+		return
+	}
+
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	if err := app.Mailer.SendSMTPMessage(msg); err != nil {
+		app.errorJSON(w, r, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "sent to " + requestPayload.To,
+	}
+
+	app.writeJSON(w, r, http.StatusAccepted, payload)
+}
--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "8080"
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -24,4 +29,21 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		panic(err)
 	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi("MAIL_PORT")
+
+	mailer := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("MAIL_FROM_NAME"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDERSS"),
+	}
+
+	return mailer
 }
