21fce0b46449d29d812dcc3c4ae01553beba19d1
tepavcevic <djordje.tepa@gmail.com>
2023-12-16 19:01:22 +0100

    Adding test btn to the front end,  handler to the broker

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -11,6 +11,7 @@ type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
 	Log    LogPayload  `json:"log,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
 }
 
 type AuthPayload struct {
@@ -21,6 +22,13 @@ type AuthPayload struct {
 type LogPayload struct {
 	Name string `json:"name"`
 	Data string `json:"data"`
+}
+
+type MailPayload struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -49,8 +57,10 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.authenticate(w, r, requestPayload.Auth)
 	case "log":
 		app.logItem(w, r, requestPayload.Log)
+	case "mail":
+		app.mail(w, r, requestPayload.Mail)
 	default:
-		app.errorJSON(w, r, errors.New("unknown actiont"))
+		app.errorJSON(w, r, errors.New("unknown action"))
 	}
 }
 
@@ -128,6 +138,39 @@ func (app *Config) authenticate(w http.ResponseWriter, r *http.Request, a AuthPa
 	payload.Error = false
 	payload.Message = "Authenticated!"
 	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, r, http.StatusAccepted, payload)
+}
+
+func (app *Config) mail(w http.ResponseWriter, r *http.Request, m MailPayload) {
+	jsonData, _ := json.MarshalIndent(m, "", "\t")
+
+	mailServiceURL := "http://mail-service:8080/send"
+
+	request, err := http.NewRequest("POST", mailServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, r, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, r, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, r, errors.New("error calling mail service"), http.StatusInternalServerError)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "mail sent to " + m.To
 
 	app.writeJSON(w, r, http.StatusAccepted, payload)
 }
