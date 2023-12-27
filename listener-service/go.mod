e290c2179625de29bcbdd579423944665763cc1a
tepavcevic <djordje.tepa@gmail.com>
2023-12-27 18:37:12 +0100

    Add listener service with RabbitMQ

---
+++ listener-service/go.mod
@@ -0,0 +1,5 @@
+module github.com/tepavcevic/microservices-golang/listener
+
+go 1.21.5
+
+require github.com/rabbitmq/amqp091-go v1.9.0
---
+++ listener-service/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/tepavcevic/microservices-golang/listener/event"
+)
+
+func main() {
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+
+	log.Println("Listening and consuming RabbitMQ messages...")
+
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
+
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backoff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("RabbitMQ not redy yet.")
+			counts++
+		} else {
+			fmt.Println("RabbitMQ connected.")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backoff)
+		continue
+	}
+
+	return connection, nil
+}
